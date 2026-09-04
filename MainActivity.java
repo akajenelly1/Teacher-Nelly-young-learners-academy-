1f312bb090cb489b345797d25ac4fd4e95f56faa
akajenelly1 <thedigitalshelfnelz@gmail.com>
2026-09-04 20:05:22 +0100

    Refactor MainActivity to extend AppCompatActivity

--- MainActivity.java
+++ MainActivity.java
@@ -1,56 +1,26 @@
 package com.teachernelly.younglearners;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
-public class MainActivity extends Activity {
-    private static final String ACADEMY_URL = "https://teachernellyacademyap.netlify.app/";
-    private WebView webView;
+import androidx.appcompat.app.AppCompatActivity;
 
-    @SuppressLint("SetJavaScriptEnabled")
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        webView = new WebView(this);
+        WebView webView = new WebView(this);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        webView.loadUrl("https://teachernellyacademyap.netlify.app/");
+
         setContentView(webView);
-
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setDatabaseEnabled(true);
-        settings.setAllowFileAccess(false);
-        settings.setAllowContentAccess(false);
-        settings.setBuiltInZoomControls(false);
-        settings.setDisplayZoomControls(false);
-        settings.setMediaPlaybackRequiresUserGesture(false);
-
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                return false;
-            }
-        });
-
-        webView.loadUrl(ACADEMY_URL);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
     }
 }
